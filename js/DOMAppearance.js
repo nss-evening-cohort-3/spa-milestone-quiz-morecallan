6cba12932a6eaf5fdf41b00ef71cec8517828620
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-19 09:13:39 -0500

    about text reflects input

--- js/DOMAppearance.js
+++ js/DOMAppearance.js
@@ -3,13 +3,31 @@ var Carlot = (function(oldCarlot) {
         //A car DOM element that was clicked on.
         //A color name.
     
-    oldCarlot.resetBorder = function() {
+    oldCarlot.resetBorder = function(currentCard, borderColor) {
         // One function resets the border thickness and background color for each car element back to the original values.
+        var carsOnDom = document.getElementsByClassName("myCarCard");
+        for (var i = 0; i < carsOnDom.length; i++) {
+            carsOnDom[i].classList.remove("selected");
+        }
+       
     };
 
     oldCarlot.addBorder = function(clicked, color) {
         // The other function changes the thickness of the border of a car element, and changes its background color. The function must accept two arguments.
+        oldCarlot.resetBorder();
+        clicked.classList.add("selected");
+    }
+
+    oldCarlot.editText = function() {
+        var descriptionValue = document.getElementById("editDescription").value;
+        var currentCard = document.getElementsByClassName("selected")[0];
+        var theParagraph = currentCard.getElementsByTagName("p")[0];
+        theParagraph.innerHTML = descriptionValue;
     }
 
     return oldCarlot
-}(Carlot || {}));
+}(Carlot || {})); // console.log("currentCard", carsOnDom);
+        // for (var j = 0; j < carsOnDom.length; j++) {
+        //     alert("hello");
+        //     carsOnDom[j].style.border = "2px solid" + borderColor
+        // }
--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -2,6 +2,24 @@ var Carlot = (function(oldCarlot) {
 
     oldCarlot.activateEvents = function() {
         // The second IIFE should augment the original one with a function that creates all of the eventHandlers that you need for the application
+        var carsArray = document.getElementsByClassName("myCarCard");
+        for (var i = 0; i < carsArray.length; i++) {
+            carsArray[i].addEventListener("click", function(e, color) {
+                var clickedItem = e.currentTarget;
+                oldCarlot.addBorder(clickedItem, color);
+                textChangeUp(clickedItem)
+
+            })
+        }
+
+        function textChangeUp(selectedCard) {
+            var currentCard = selectedCard;
+            console.log("currentCard", currentCard);
+            var textInput = document.getElementById("editDescription");
+            textInput.addEventListener("keyup", function(e, currentCard) {
+                Carlot.editText(e, currentCard);
+            })
+        }
     }
 
     return oldCarlot
